a43a3f108094a566a444274dcc1b912a444d5d4c
Caladan <94300963+Caladan08@users.noreply.github.com>
2022-09-05 20:58:11 +0530

    Added the purple theme color to tailwind.config.js

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple": "#b33dd1"
+      }
+    },
   },
   plugins: [],
 }
