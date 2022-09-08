4a2b3452150b7a2490e0880df05c4f6db7b756e1
Caladan <94300963+Caladan08@users.noreply.github.com>
2022-09-08 20:07:10 +0530

    Simplified the validation process in /login and other changes

--- src/routes/(frontPage)/login/+page.svelte
+++ src/routes/(frontPage)/login/+page.svelte
@@ -1,16 +1,14 @@
 <script>
 	import { goto } from '$app/navigation';
-	let usernameValue = "", usernameError, passwordValue = "", passwordError;
+	let usernameValue = '',
+		usernameError,
+		passwordValue = '',
+		passwordError;
+  $: usernameValid = usernameValue.length === 0
+  $: passwordValid = passwordValue.length === 0
 	function login() {
-    if (usernameValue.length === 0 || passwordValue.length === 0) {
-      return
-    }
 		goto('/dashboard');
 	}
-  function inputChanged(e) {
-    usernameValue.length === 0 ? usernameError = "Please enter a username!" : usernameError = null
-    passwordValue.length === 0 ? passwordError = "Please enter a password!" : passwordError = null
-  }
 </script>
 
 <svelte:head>
@@ -18,42 +16,40 @@
 </svelte:head>
 
 <h1 class="font-bold text-3xl text-white my-10" align="center">Nuntia</h1>
-
-<div class="w-1/4 bg-white mx-auto h-[50vh] rounded-sm p-10" align="center">
+<div align="center" class="w-[20rem] bg-white mx-auto h-[50vh] rounded-sm p-10">
 	<form on:submit|preventDefault={login}>
-    <p align="center" class="mb-6 mt-0">Login to continue!</p>
-    <label for="username">Username: </label>
-    <div data-error={usernameError} class="input-container">
-      <input
-        type="text"
-        id="username"
-        class="w-full rounded-md px-2.5 mb-5 h-[5vh] mt-2"
-        placeholder="Your username..."
-        autocomplete="off"
-          required
-        bind:value={usernameValue}
-          on:blur={inputChanged}
-      />
-    </div>
-    <label for="password">Password: </label>
-    <div data-error={passwordError} class="input-container">
-      <input
-        type="password"
-        id="password"
-        class="w-full rounded-md px-2.5 h-[5vh] mt-2"
-        placeholder="Your password..."
-          required
-        bind:value={passwordValue}
-          on:blur={inputChanged}
-      />
-    </div>
-    <button
-      class="w-full my-10 h-10 rounded-sm border-2 border-solid border-purple hover:bg-purple transition-all duration-150 ease-in"
-        type="submit"
-      >Login</button
-    >
-    <hr class="border-[0.25px] border-solid border-black" />
-  </form>
+		<p align="center" class="mb-6 mt-0">Login to continue!</p>
+		<label for="username">Username: </label>
+		<div data-error={usernameError} class="input-container">
+			<input
+				type="text"
+				id="username"
+				class="w-full rounded-md px-2.5 mb-5 h-[5vh] mt-2"
+				placeholder="Your username..."
+				required
+				bind:value={usernameValue}
+        class:invalid="{usernameValid}"
+			/>
+		</div>
+		<label for="password">Password: </label>
+		<div data-error={passwordError} class="input-container">
+			<input
+				type="password"
+				id="password"
+				class="w-full rounded-md px-2.5 h-[5vh] mt-2"
+				placeholder="Your password..."
+				required
+				bind:value={passwordValue}
+        class:invalid="{passwordValid}"
+        autocomplete
+			/>
+		</div>
+		<button
+			class="w-full my-10 h-10 rounded-sm border-2 border-solid border-purple hover:bg-purple transition-all duration-150 ease-in"
+			type="submit">Login</button
+		>
+		<hr class="border-[0.25px] border-solid border-black" />
+	</form>
 </div>
 
 <style>
@@ -61,23 +57,12 @@
 	* {
 		font-family: 'Montserrat', sans-serif;
 	}
-	input[type='text'] {
+	input {
 		border: 2px solid #ccc;
 		outline: none;
 		transition: 0.5s;
 	}
-  input[type='password'] {
-		border: 2px solid #ccc;
-		outline: none;
-		transition: 0.5s;
-	}
-	input[type='text']:focus {
-		border: 2px solid #b33dd1;
-	}
-  input[type='password']:focus {
-		border: 2px solid #b33dd1;
-	}
-	.input-container[data-error] input {
+	.invalid {
 		border-color: red;
 	}
 </style>
