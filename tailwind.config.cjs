067867ca632f2f145a4a0c8a54421c7f234594ea
Caladan <94300963+Caladan08@users.noreply.github.com>
2022-09-10 12:05:10 +0530

    Switched up the colors and added the dominant, complementary and accent

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "purple": "#b33dd1",
+        "purple": "#4526D3",
         "bgColor": "#1E1E1E",
-        "white": "#D9D9D9"
+        "white": "#D9D9D9",
+        "dominantColor": "#4526D3",
+        "accent": "#1E1926",
+        "complementary": "#FDF7FF"
+      },
+      backgroundImage: {
+        "heroImage": "url('./static/images/stockOne.png')"
       }
     },
   },
