0996e83df786766a08f48de7a7e4fcbddf1f9399
Caladan <94300963+Caladan08@users.noreply.github.com>
2022-09-13 16:38:27 +0530

    Fixed bug in CommonJS module import

--- src/hooks.js
+++ src/hooks.js
@@ -1,14 +1,14 @@
-import { connection, connect } from "mongoose";
+import mongoose from "mongoose";
 import "dotenv/config"
 
 export async function handle({ event, resolve }) {
-  console.log(connection.readyState)
-  if (connection.readyState === 1) {
+  console.log(mongoose.connection.readyState)
+  if (mongoose.connection.readyState === 1) {
     const response = await resolve(event);
     return response;
-  } else if (connection.readyState === 0) {
+  } else if (mongoose.connection.readyState === 0) {
     const mongoURI = process.env.DB_URI.replace("<password>", encodeURIComponent(process.env.DB_PASSWORD))
-    await connect(mongoURI, {
+    await mongoose.connect(mongoURI, {
       keepAlive: true,
     })
     const response = await resolve(event);
